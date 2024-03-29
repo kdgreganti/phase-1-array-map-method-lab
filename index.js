6247c6d55b3e8f1d21dedac8a1752615c2603449
Kyle Greganti <kyle.greganti@yahoo.com>
2024-03-29 14:51:44 -0500

    Meets Criteria

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const titleCasedNames = tutorials.map((name) => {
+    const words = name.split(" ");
+    const titleCasedWords = words.map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    });
+    return titleCasedWords.join(" ");
+  });
+  return titleCasedNames;
+};
+
